d6c8e5b43baa6b7fe18163745a3b39942a5bae69
haxnovr <mayankgajwe@gmail.com>
2021-10-28 15:38:40 +0530

    test

---
+++ CLab 22-10-21/prg1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 6;
+    printf("int a = %d & int b = %d", a, b);
+    return 0;
+}
---
+++ CLab 22-10-21/prg3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 6;
+    int b = 2;
+    printf("Addition of a & b = %d \n", a+b);
+    printf("Subtraction of a & b = %d \n", a-b);
+    printf("Multiplication of a & b = %d \n", a*b);
+    printf("Division of a & b = %d \n", a/b);
+    return 0;
+}
