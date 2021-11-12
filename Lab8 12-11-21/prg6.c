8fe9b11596d11ce5d5356e10b2809518b7ecec8e
haxnovr <mayankgajwe@gmail.com>
2021-11-12 16:49:58 +0530

    init

--- Lab8 12-11-21/prg6.c
+++ Lab8 12-11-21/prg6.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
+#include<math.h>
+
+#define s(x) (x)*(x)
 
 int main(){
-    float m1,m2;
+    float a,b,c;
 
-    printf("Enter the values of m1 and m2 = ");
-    scanf("%f %f", &m1, &m2);
+    printf("Enter the values of sides a, b and c = ");
+    scanf("%f %f %f", &a, &b, &c);
 
-    int t = m1 == m2;
+    int t = (a==sqrt(s(b)+s(c))) || (b==sqrt(s(a)+s(c))) || (c==sqrt(s(b)+s(a)));
 
     printf("%d", t);
     return 0;
