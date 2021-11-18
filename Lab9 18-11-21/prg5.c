b08bebdeca6b4a986729e63dc79621959df68924
haxnovr <mayankgajwe@gmail.com>
2021-11-18 17:48:07 +0530

    init

--- Lab9 18-11-21/prg5.c
+++ Lab9 18-11-21/prg5.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
 int main(){
-    int bs
+    int bs, hra, da, gs;
+    printf("Enter the salary: ");
+    scanf("%d", &bs);
+    if(bs<1500){
+        hra = bs*10/100;
+        da = bs*40/100;
+    }
+    else{
+        hra = 500;
+        da = bs*50/100;
+    }
+    gs = bs+hra+da;
+    printf("For the salary %d\nhra = %d\nda = %d\ngross salary = %d",bs,hra,da,gs);
 }
