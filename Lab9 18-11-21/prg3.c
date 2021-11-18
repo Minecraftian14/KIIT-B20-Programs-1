471638d50dfd4e7c6fc0b35c2284effdda395143
haxnovr <mayankgajwe@gmail.com>
2021-11-18 17:22:44 +0530

    init

--- Lab9 18-11-21/prg3.c
+++ Lab9 18-11-21/prg3.c
@@ -4,5 +4,13 @@ int main(){
     printf("Enter the resistances in a wheatsones bridge: ");
     scanf("%f %f %f %f", &p,&q,&r,&s);
 
-    if(p)
+    if(p*s == r*q){
+        printf("\nThe Wheatstone bridge is balanced");
+    }
+    else{
+        printf("The wheatstone bridge is NOT balanced");
+        s = r*q/p;
+        printf("\nChoose S = %f to balance the bridge", s);
+    }
+return 0;
 }
