ddec0d2d5d78ec59ffe412249d75c3a561ff84e2
haxnovr <mayankgajwe@gmail.com>
2021-11-21 13:02:23 +0530

    init

--- Lab10 20-11-21/prg16.c
+++ Lab10 20-11-21/prg16.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
-
-int main(){
-    printf("Enter a sentence");
-    return 0;
+int main()
+{
+	char a;
+	printf("Enter an alphabet in any case:");
+	scanf("%c",&a);
+	if ((a>=97) && (a<=122))
+		{
+		a=a-32;
+		printf("Character in upper case: %c\n",a);
+		}
+	else if ((a>=65) && (a<=90))
+		{
+		a=a+32;
+		printf("Character in lower case: %c\n",a);
+		}
+	return 0;
 }
